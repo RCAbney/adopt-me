524d67869b1726451aa8285855ba8126d5004d3b
RCAbney <ryan@rcabney.com>
2019-06-24 14:35:25 -0400

    add emotion css - navbar component - styling - animations

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 import ThemeContext from "./ThemeContext";
+import NavBar from "./NavBar";
 
 const App = () => {
-  const themeHook = useState("darkblue");
+  const themeHook = useState("rebeccapurple");
 
   return (
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <header>
-            <Link to="/">Adopt Me</Link>
-          </header>
+          <NavBar />
           <Router>
             <SearchParams path="/" />
             <Details path="/details/:id" />
